07aaa58b968d3d43f3dc8bdf96c14a326ee441ad
nonusae <nonusae@gmail.com>
2021-03-24 17:13:34 +0700

    Refactor to use class instead of component for perspective background

--- src/components/BackgroundOverlay/BackgroundOverlay.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import styles from './BackgroundOverlay.module.css';
-
-const BackgroundOverlay = ({ children }) => {
-  return <div className={styles.backgroundOverlay}>{children}</div>;
-};
-
-export default BackgroundOverlay;
